624ea2b2bc887f802c953f37b934ab7fdf030d51
Luis Eduardo <lerivadeneira@pucesi.edu.ec>
2023-10-08 17:34:49 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+
+const main = async () => {
+    let opt;
+
+    do {
+
+        opt = await inquirerMenu();
+
+        await pausa();
+
+    } while (opt != 0)
+}
+
+main();
